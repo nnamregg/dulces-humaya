e8ff5b7cb8366ecdaeb5d5a53747555e0c6a48e4
German Troncoso <nnamreg.t@gmail.com>
2023-06-20 19:54:51 -0300

    Correcciones en atributos de componente Contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
                 </div>
             </Container>
             <div className="map">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15440.418668555847!2d-91.24363303473288!3d14.64999952347411!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x858eca9f2c33ff1b%3A0x675fcc14183f5786!2sIsla%20de%20Los%20Gatos!5e0!3m2!1ses-419!2sar!4v1686940210883!5m2!1ses-419!2sar" width="100%" height="450" style={{border:0}} allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15440.418668555847!2d-91.24363303473288!3d14.64999952347411!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x858eca9f2c33ff1b%3A0x675fcc14183f5786!2sIsla%20de%20Los%20Gatos!5e0!3m2!1ses-419!2sar!4v1686940210883!5m2!1ses-419!2sar" width="100%" height="450" style={{border:0}} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
             </div>
             <Container>
                 <Row className="mt-5">
@@ -55,19 +55,19 @@ const Contact = () => {
                         <form action="" className="w-100">
                             <Row>
                                 <Col md={6} className="form-group">
-                                    <input type="text" name="name" class="form-control" id="name" placeholder="Nombre" required></input>
+                                    <input type="text" name="name" className="form-control" id="name" placeholder="Nombre" required></input>
                                 </Col>
                                 <Col md={6} className="form-group mt-3 mt-md-0">
-                                    <input type="email" class="form-control" name="email" id="email" placeholder="Email" required></input>
+                                    <input type="email" className="form-control" name="email" id="email" placeholder="Email" required></input>
                                 </Col>
                             </Row>
                             <div className="form-group mt-3">
-                                <input type="text" class="form-control" name="subject" id="subject" placeholder="Asunto" required></input>
+                                <input type="text" className="form-control" name="subject" id="subject" placeholder="Asunto" required></input>
                             </div>
                             <div className="form-group mt-3">
-                                <textarea class="form-control" name="message" rows="8" placeholder="Mensaje" required></textarea>
+                                <textarea className="form-control" name="message" rows="8" placeholder="Mensaje" required></textarea>
                             </div>
-                            <div class="text-center my-3">
+                            <div className="text-center my-3">
                                 <Button type="submit" className="fw-semibold">Enviar</Button>
                             </div>
                         </form>
