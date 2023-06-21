f884ecb6249531cd31ea403a0090b51c37584d2e
German Troncoso <nnamreg.t@gmail.com>
2023-06-20 23:28:30 -0300

    Agrego Modal de receta

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,10 +1,24 @@
-import { Container, Row, Col, Image } from 'react-bootstrap';
-import recipe1Img from "../assets/recipes/recipes-01.jpg";
-import recipe2Img from "../assets/recipes/recipes-02.png";
-import recipe3Img from "../assets/recipes/recipes-03.jpg";
+import { useState } from "react";
+import { recipes } from "../helpers/recipes.js";
+import { Container, Row, Col, Image, Modal, ListGroup, CloseButton } from "react-bootstrap";
+import { IoPeople, IoTimer, IoExtensionPuzzle } from "react-icons/io5";
 
 const Recipes = () => {
+    const [showModal, setShowModal] = useState(false);
+    const [maxRecipe, setMaxRecipe] = useState(null);
+
+    const handleClick = (recipe) => {
+        setMaxRecipe(recipe);
+        setShowModal(true);
+    }
+
+    const handleHide = () => {
+        setShowModal(false);
+        setMaxRecipe(null);
+    }
+
     return (
+        <>
         <section id="recipes" className="recipes py-5">
             <Container>
                 <div className="section-title pb-4 text-start">
@@ -12,44 +26,84 @@ const Recipes = () => {
                     <p className="golden fw-bolder">Las recetas más ricas</p>
                 </div>
                <Row>
-                    <Col lg={4} md={6}>
-                        <div className="recipe-card text-center overflow-hidden mb-5">
-                            <Image fluid src={recipe1Img} alt="Alfajores de Dulce de Leche" />
-                            <div className="recipe-info text-light position-absolute">
-                                <div className="recipe-info-content">
-                                    <h4 className="fs-5">Alfajores de Dulce de Leche</h4>
-                                    <span>something something</span>
+                    { recipes.map((recipe) => {
+                        return (
+                            <Col lg={4} md={6} key={recipe.img.slice(0,-4)}>
+                                <div className="recipe-card text-center overflow-hidden mb-5" onClick={() => handleClick(recipe)}>
+                                    <Image fluid src={`/recipes/${recipe.img}`} alt={recipe.title} />
+                                    <div className="recipe-info text-light position-absolute">
+                                        <div className="recipe-info-content">
+                                            <h4>{recipe.title}</h4>
+                                            <div className="d-flex justify-content-around w-75 mx-auto">
+                                                <div className="d-flex align-items-center">
+                                                    <IoPeople className="me-2" />
+                                                    <span>{recipe.servings}</span>
+                                                </div>
+                                                <div className="d-flex align-items-center">
+                                                    <IoTimer className="me-2" />
+                                                    <span>{recipe.prepTime}' mins</span>
+                                                </div>
+                                                <div className="d-flex align-items-center">
+                                                    <IoExtensionPuzzle className="me-2" />
+                                                    <span>{recipe.difficulty}</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                    </Col>
-
-                    <Col lg={4} md={6}>
-                        <div className="recipe-card text-center overflow-hidden mb-5">
-                            <Image fluid src={recipe2Img} alt="Crepes de Choconutt" />
-                            <div className="recipe-info text-light position-absolute">
-                                <div className="recipe-info-content">
-                                    <h4 className="fs-5">Crepes de Choconutt</h4>
-                                    <span>something something</span>
-                                </div>
-                            </div>
-                        </div>
-                    </Col>
-
-                    <Col lg={4} md={6}>
-                        <div className="recipe-card text-center overflow-hidden mb-5">
-                            <Image fluid src={recipe3Img} alt="Brownies veganos" />
-                            <div className="recipe-info text-light position-absolute">
-                                <div className="recipe-info-content">
-                                    <h4 className="fs-5">Brownies veganos</h4>
-                                    <span>something something</span>
-                                </div>
-                            </div>
-                        </div>
-                    </Col>
+                            </Col>
+                        )
+                    })}
                </Row>
-            </Container>            
+            </Container>
         </section>
+        <Modal
+            show={showModal}
+            onHide={handleHide}
+            scrollable={true}
+            dialogClassName="custom-modal"
+            aria-labelledby="custom-modal-title"
+        >
+            <Modal.Header>
+                <Modal.Title id="custom-modal-title">
+                    {maxRecipe?.title}
+                </Modal.Title>
+                <CloseButton variant="white" onClick={() => setShowModal(false)} />
+            </Modal.Header>
+
+            <Modal.Body>
+                <Image fluid src={`recipes/${maxRecipe?.imgWide}`} />
+                <div className="modal-recipe-info d-flex justify-content-around my-4 mx-auto text-light">
+                    <div className="d-flex flex-column align-items-center">
+                        <IoPeople className="mb-2"/>
+                        <span>{maxRecipe?.servings}</span>
+                    </div>
+                    <div className="d-flex flex-column align-items-center">
+                        <IoTimer className="mb-2"/>
+                        <span>{maxRecipe?.prepTime}' mins</span>
+                    </div>
+                    <div className="d-flex flex-column align-items-center">
+                        <IoExtensionPuzzle className="mb-2"/>
+                        <span>{maxRecipe?.difficulty}</span>
+                    </div>
+                </div>
+                
+                <h5 className="mt-4 mb-2 golden">Ingredientes</h5>
+                <ListGroup variant="flush">
+                    { maxRecipe?.ingredients.map((ingredient) => {
+                        return <ListGroup.Item>{ingredient}</ListGroup.Item>
+                    })}
+                </ListGroup>
+
+                <h5 className="mt-4 mb-2 golden">Instrucciones</h5>
+                <ListGroup as="ol" variant="flush" numbered>
+                    { maxRecipe?.instructions.map((instruction) => {
+                        return <ListGroup.Item>{instruction}</ListGroup.Item>
+                    })}
+                </ListGroup>
+            </Modal.Body>
+        </Modal>
+        </>
     )
 }
 
---
+++ src/helpers/recipes.js
@@ -0,0 +1,88 @@
+export const recipes = [
+    {
+        title: "Alfajores de Dulce de Leche",
+        img: "recipes-01.png",
+        imgWide: "recipes-01_wide.png",
+        prepTime: 30,
+        servings: 12,
+        difficulty: "Fácil",
+        ingredients: [
+            "200 g de harina de trigo",
+            "100 g de maicena",
+            "150 g de mantequilla a temperatura ambiente",
+            "100 g de azúcar impalpable",
+            "1 cucharadita de esencia de vainilla",
+            "Dulce de leche para rellenar",
+            "Coco rallado (opcional) para decorar"
+        ],
+        instructions: [
+            "Precalienta el horno a 180°C.",
+            "En un tazón, mezcla la harina, la maicena y el azúcar impalpable.",
+            "Agrega la mantequilla y la esencia de vainilla, y mezcla hasta obtener una masa suave y homogénea.",
+            "Extiende la masa sobre una superficie enharinada y corta círculos del tamaño deseado.",
+            "Coloca los círculos de masa en una bandeja para horno y hornea durante aproximadamente 12-15 minutos, o hasta que estén ligeramente dorados.",
+            "Retira del horno y deja enfriar completamente.",
+            "Une dos galletitas con una generosa cantidad de dulce de leche.",
+            "Si lo deseas, puedes espolvorear coco rallado sobre los bordes del dulce de leche.",
+            "¡Disfruta de estos deliciosos alfajores de dulce de leche caseros!"
+        ]
+    },
+    {
+        title: "Crepes de Choconutt",
+        img: "recipes-02.png",
+        imgWide: "recipes-02_wide.png",
+        prepTime: 20,
+        servings: 6,
+        difficulty: "Media",
+        ingredients: [
+            "1 taza de harina de trigo",
+            "2 huevos",
+            "1/2 taza de leche",
+            "1/2 taza de agua",
+            "2 cucharadas de azúcar",
+            "2 cucharadas de mantequilla derretida",
+            "Choconutt para rellenar"
+        ],
+        instructions: [
+            "En un tazón grande, mezcla la harina, los huevos, la leche, el agua, el azúcar y la mantequilla derretida.",
+            "Bate la mezcla hasta obtener una masa suave y sin grumos.",
+            "Calienta una sartén antiadherente a fuego medio y engrásala ligeramente con mantequilla.",
+            "Vierte aproximadamente 1/4 de taza de masa en la sartén y gira la sartén rápidamente para cubrir toda la superficie con una capa delgada de masa.",
+            "Cocina el crepe durante aproximadamente 2 minutos, o hasta que esté dorado en la parte inferior.",
+            "Voltea el crepe con cuidado y cocina el otro lado durante otros 1-2 minutos.",
+            "Repite el proceso con el resto de la masa.",
+            "Una vez que todos los crepes estén listos, extiende una generosa cantidad de Choconutt en el centro de cada uno.",
+            "Dobla los crepes en forma de triángulo o simplemente enróllalos.",
+            "Sirve los crepes con Choconutt y disfruta de esta irresistible combinación."
+        ]
+    },
+    {
+        title: "Brownies Veganos",
+        img: "recipes-03.png",
+        imgWide: "recipes-03_wide.png",
+        prepTime: 45,
+        servings: 8,
+        difficulty: "Difícil",
+        ingredients: [
+            "1 taza de harina de trigo integral",
+            "1/2 taza de cacao en polvo sin azúcar",
+            "1/2 taza de azúcar moreno",
+            "1/4 de taza de aceite vegetal",
+            "1/4 de taza de leche de almendras (o cualquier leche vegetal)",
+            "1 cucharadita de esencia de vainilla",
+            "1/2 cucharadita de polvo para hornear",
+            "Una pizca de sal",
+            "Nueces picadas (opcional)"
+        ],
+        instructions: [
+            "Precalienta el horno a 180°C.",
+            "En un tazón grande, mezcla la harina de trigo integral, el cacao en polvo, el azúcar moreno, el polvo para hornear y la sal.",
+            "Agrega el aceite vegetal, la leche de almendras y la esencia de vainilla. Mezcla bien hasta obtener una masa suave y homogénea.",
+            "Si lo deseas, puedes agregar nueces picadas a la masa y mezclar.",
+            "Vierte la masa en un molde cuadrado o rectangular previamente engrasado.",
+            "Hornea durante aproximadamente 20-25 minutos, o hasta que al insertar un palillo en el centro, salga ligeramente húmedo.",
+            "Retira del horno y deja enfriar completamente antes de cortar en porciones.",
+            "¡Disfruta de estos deliciosos brownies veganos, ideales para satisfacer tus antojos de chocolate sin ingredientes de origen animal!"
+        ]
+    }
+]
